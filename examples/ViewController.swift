0db7ac98080436c68c86ed0122d6db903d413d92
Flavio Franco <vleix.92@gmail.com>
2019-11-12 00:35:21 -0500

    first commit

--- examples/ViewController.swift
+++ examples/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let arrayTopics = ["Animation", "Constraints by Code", "Networking native"]
 
+    @IBOutlet weak var tblTopics: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
     }
+    
+    func setupTableView() {
+        self.tblTopics.delegate = self
+        self.tblTopics.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayTopics.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
 
-
+        cell.textLabel?.text = arrayTopics[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
-
